1418aaafb07e9ba28deeb81451bea606d34ecd55
Casey Glidewell <caseyglidewell@gmail.com>
2023-01-07 15:20:11 -0800

    added second image, meeting basic requirements for today's lab class.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class App extends React.Component {
       <>
         <Header />
 
-        <Main title={"mainMenu"} imageUrl={"https://img.itch.zone/aW1hZ2UvMjg0NDYwLzEzODYwMjkucG5n/347x500/0mLE8Q.png"} description={"The title screen for Nest Defender The First: The Start Of An Empire"} />
+        <Main title={"Nest Defender Title Screen"} imageUrl={"https://img.itch.zone/aW1hZ2UvMjg0NDYwLzEzODYwMjkucG5n/347x500/0mLE8Q.png"} description={"The title screen for Nest Defender The First: The Start Of An Empire"} />
+
+        <Main title={"Astronaut Gameplay"} imageUrl={"https://img.itch.zone/aW1hZ2UvMjE1Mzc3LzEwMTc2NjIuZ2lm/347x500/sY2mzc.gif"} description={"A gif image of an astronaut in a 2D game propelling themselves with a shotgun's recoil."} />
 
         <Footer />
       </>
