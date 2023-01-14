d42ca31f676dd6e3ac38f6355f69861e7b95f487
Casey Glidewell <caseyglidewell@gmail.com>
2023-01-14 14:25:29 -0800

    first draft of modal working, provides title, image description, and can be dismissed by clicking the x or off the modal.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,102 @@
 import React from "react";
-import Student from "./Student";
 import Header from "./Header";
-import Main from "./Main";
 import Footer from "./Footer";
 import data from "./data.json";
 import HornedBeast from "./HornedBeast";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+//!NOT IN USE
+//!import Student from "./Student";
+//!import Main from "./Main";
 
 
-
-
+//HIERARCHY: lower number is a higher parent.
+//index.js 1
+//App.js 2
+//Header 3
+//HornedBeast 3
+//Footer 3
 
 class App extends React.Component {
+
+  constructor(props) {
+
+    super(props)
+
+    this.state = {
+
+      clickedFavorite: false,
+      showingModal: false,
+      cardName: '',
+      cardDescription: '',
+      cardImage: '',
+      totalFavorites: 0,
+
+    };
+  };
+
+  handleShowModal = (name, image, description) => {
+    this.setState({
+      showModal: true,
+      cardName: name,
+      cardImage: image,
+      cardDescription: description
+    })
+  }
+
+  handleCloseModal = () => {
+    this.setState({
+      showModal: false
+    })
+  }
+
+
+
+
+
   render() {
 
     return (
       <>
-        <Header/>
-        
+        <Header />
+
         {data.map(beasts => <HornedBeast
+
           title={beasts.title}
           imageUrl={beasts.image_url}
           description={beasts.description}
           key={beasts._id}
-        />)}
-      
-        <Footer/>
+          handleShowModal={this.handleShowModal}
+
+        />
+
+        )}
+
+        <Modal
+
+          show={this.state.showModal}
+
+          onHide={this.handleCloseModal}>
+
+          <Modal.Header closeButton>
+
+            <Modal.Title>{this.state.cardName}</Modal.Title>
+
+          </Modal.Header>
+
+          <Modal.Body>
+
+            <img src={this.state.cardImage} alt={this.state.cardDescription} height={300} />
+
+            {this.state.cardDescription}
+
+          </Modal.Body>
+
+        </Modal>
+
+        <Footer />
+
       </>
 
 
--- src/Student.js
+++ src/Student.js
@@ -1,15 +1,18 @@
-import React from "react";
+//NOT IN USE
 
-class Student extends React.Component {
-  render() {
-    return (
-      <>
-        <h2>{this.props.name}</h2>
-        <p>Favorite Fruit: {this.props.fruit}</p>
-      </>
-    )
-  }
-}
 
-export default Student
+// import React from "react";
+
+// class Student extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <h2>{this.props.name}</h2>
+//         <p>Favorite Fruit: {this.props.fruit}</p>
+//       </>
+//     )
+//   }
+// }
+
+// export default Student
 //All above was following along with Roger Reyes in class, and was used as a template for the other .js files.
